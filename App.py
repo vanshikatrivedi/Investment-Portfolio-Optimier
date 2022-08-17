635cd43d27cbd6624783ed01dba7c141664e6f34
Vanshika <vanshika13574@gmail.com>
2022-08-17 18:45:58 +0000

    Update App.py

--- App.py
+++ App.py
@@ -7,10 +7,6 @@ import yfinance as yf
 
 
 st.set_page_config(layout="wide")
-
-
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
     
 
 st.title('INVESTMENT PORTFOLIO OPTIMISER')
