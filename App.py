0405715b1993f2847672805278f53ce83b5d4e3a
Vanshika <vanshika13574@gmail.com>
2022-08-18 08:28:19 +0000

    removed matplotlib

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 import yfinance as yf
 
